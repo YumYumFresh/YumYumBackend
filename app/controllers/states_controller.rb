b9e8b70b92cabd7a2dcea8286110c218cc617f68
metrio <dlima.nyit@gmail.com>
2021-04-23 16:39:52 -0400

    with comments

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,6 +1,5 @@
 class StatesController < ApplicationController
     
-
     def index
         states = State.all.includes(:produces)
         render json: states, except: [:created_at, :updated_at]
@@ -11,11 +10,11 @@ class StatesController < ApplicationController
         render json: state, except: [:created_at, :updated_at]
     end
 
-
+    ## This method finds Produces associated by state, request being sent gives us :name, :month, and :lookup_id
     def states_produce
         produces = State.find_by(:name => params[:name]).by_months(params[:month])
-        render json: produces, except: [:created_at, :updated_at], lookup_id: params[:lookup_id]
-        #produces, serializer: ProduceSerializer, only_associated_harvest: params[:lookup_id], status: :ok
+    #For each of these produces in this array, we plug in lookup_id into the serializer so that we can narrow down the associated info
+        render json: produces, lookup_id: params[:lookup_id]
     end
 
 
--- app/models/state.rb
+++ app/models/state.rb
@@ -2,17 +2,18 @@ class State < ApplicationRecord
     has_many :harvests
     has_many :produces, through: :harvests
 
+    #In this method we find all the harvest joiner tables that include the month from the request
     def by_months(current_month)
         produce_id_array = []
 
+    #Here we shovel all produce_ids into the array so we can narrow down which produce to pull
         self.harvests.each{ |harvest| 
             if harvest.months.include?(current_month)
                 produce_id_array << harvest.produce_id
             end
         }
-        
+    #We then select all the produce_ids that are in that array    
         produce_array = self.produces.select { |produce|
-            #produce.include_months 
             produce_id_array.include?(produce.id)
         }
 
