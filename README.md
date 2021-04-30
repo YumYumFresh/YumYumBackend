e7244b3ab8824dff03add813cd75bb01519c5032
JlordA <james.ardery@gmail.com>
2021-04-30 15:33:53 -0400

    home reroute

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# README
+# YumYum Fresh API Documentation
 
 This README would normally document whatever steps are necessary to get the
 application up and running.
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :states, only: [:index]
   get '/states/:name', to: 'states#showName'
   get '/states/:name/produces', to: 'states#states_produce'
+  root "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
